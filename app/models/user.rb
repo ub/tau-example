c4ba1141f211c7f60675d8127bf30050a66f2f9f
Yuri Baranoav <baranovu@gmail.com>
2021-02-22 22:03:55 +0300

    Add support for omniauth-telegram to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable,  :omniauth_providers => [:telegram]
+  validates_uniqueness_of :telegram_id, allow_nil: true
 end
